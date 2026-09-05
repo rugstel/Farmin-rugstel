cb8ac629bce1515ea762ff998e90335bacf7e58e
rugstel <rugstel_45@mail.ru>
2026-09-05 12:05:41 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 import os
 
 # TODO: Замените "ВАШ_ТОКЕН" на реальный токен бота, полученный от @BotFather
-BOT_TOKEN = "1234567890:ABCdefGHIjklMNOpqrsTUVwxyz"  # ← ВСТАВЬТЕ СВОЙ ТОКЕН
+BOT_TOKEN = "8257848684:AAGkZ_tllHKVE-IIHWsJerJqoAip4P4wexs"  # ← ВСТАВЬТЕ СВОЙ ТОКЕН
 
 # Определяем окружение (для Vercel)
 IS_VERCEL = os.getenv("VERCEL") == "1"
