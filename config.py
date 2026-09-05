543acd232727b25355b7d859b2ed449d4c6f6f6c
rugstel <rugstel_45@mail.ru>
2026-09-05 12:35:22 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # Токен бота берется из переменных окружения (на Render)
 # Или используется запасной токен для локального запуска
-BOT_TOKEN = os.getenv("BOT_TOKEN", "ВАШ_ТОКЕН_СЮДА")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8257848684:AAGkZ_tllHKVE-IIHWsJerJqoAip4P4wexs")
 
 # Другие настройки (опционально)
 # ADMIN_IDS = []  # Список ID администраторов
